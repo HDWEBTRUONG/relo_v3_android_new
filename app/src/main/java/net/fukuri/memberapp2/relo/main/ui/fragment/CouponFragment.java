8f338ff5e329c1bda7b8fd4224c55b5054409eb7
huytran <8rNv38OmBSV>
2017-03-21 17:00:56 +0700

    [RELO-3] init splash screen activity

--- app/src/main/java/net/fukuri/memberapp2/relo/main/ui/fragment/CouponFragment.java
+++ app/src/main/java/net/fukuri/memberapp2/relo/main/ui/fragment/CouponFragment.java
@@ -6,11 +6,6 @@ import android.view.View;
 import net.fukuri.memberapp2.relo.R;
 import net.fukuri.memberapp2.relo.framework.phvtFragment.BaseFragment;
 
-/**
- * Created by HuyTran on 3/21/17.
- * MOTE
- */
-
 public class CouponFragment extends BaseFragment {
     @Override
     public int getRootLayoutId() {
