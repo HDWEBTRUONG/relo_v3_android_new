bf94c3e2b5ed4becd2515a8479ab59602937004c
huytran <8rNv38OmBSV>
2017-03-22 16:09:10 +0700

    Added toolbar to fragments, changed color. Added recycler view library

--- app/src/main/java/net/fukuri/memberapp2/relo/main/ui/fragment/CouponAreaFragment.java
+++ app/src/main/java/net/fukuri/memberapp2/relo/main/ui/fragment/CouponAreaFragment.java
@@ -1,10 +1,14 @@
 package net.fukuri.memberapp2.relo.main.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import net.fukuri.memberapp2.relo.R;
 import net.fukuri.memberapp2.relo.framework.phvtFragment.BaseFragment;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by HuyTran on 3/21/17.
@@ -18,7 +22,9 @@ public class CouponAreaFragment extends BaseFragment {
 
     @Override
     protected void getMandatoryViews(View root, Bundle savedInstanceState) {
-
+        Toolbar toolbar = ButterKnife.findById(root, R.id.toolbar);
+        TextView title = ButterKnife.findById(toolbar, R.id.toolbar_title);
+        title.setText(R.string.title_coupon_area);
     }
 
     @Override
--- app/src/main/java/net/fukuri/memberapp2/relo/main/ui/fragment/CouponListFragment.java
+++ app/src/main/java/net/fukuri/memberapp2/relo/main/ui/fragment/CouponListFragment.java
@@ -1,33 +1,33 @@
 package net.fukuri.memberapp2.relo.main.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
 import net.fukuri.memberapp2.relo.R;
 import net.fukuri.memberapp2.relo.framework.phvtFragment.BaseFragment;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by HuyTran on 3/21/17.
  */
 
-public class CouponFragment extends BaseFragment {
-
-    @BindView(R.id.textView)
-    TextView tvCouponTitle;
+public class CouponListFragment extends BaseFragment {
 
     @Override
     public int getRootLayoutId() {
-        return R.layout.fragment_coupon;
+        return R.layout.fragment_coupon_list;
     }
 
     @Override
     protected void getMandatoryViews(View root, Bundle savedInstanceState) {
         ButterKnife.bind(this, root);
-        tvCouponTitle.setText("Hello world");
+        Toolbar toolbar = ButterKnife.findById(root, R.id.toolbar);
+        TextView title = ButterKnife.findById(toolbar, R.id.toolbar_title);
+        title.setText(R.string.title_coupon_list);
+
     }
 
     @Override
