835b8e177c06b2b07b3bdda2e1fcc351a80b8723
huytran <8rNv38OmBSV>
2017-03-23 12:04:30 +0700

    [Relo-2] Changed project structure second edition

--- app/src/main/java/bsvandroid/main/util/Common.java
+++
@@ -1,8 +0,0 @@
-package bsvandroid.main.util;
-
-/**
- * Created by HuyTran on 3/21/17.
- */
-
-public class Common {
-}
---
+++ app/src/main/java/main/ReloApp.java
@@ -0,0 +1,10 @@
+package main;
+
+import android.app.Application;
+
+/**
+ * Created by HuyTran on 3/21/17.
+ */
+
+public class ReloApp extends Application {
+}
