a9fff2be7906eb11d477473ef2de3d000332eb8a
Quy Nguyen <quynguyen@HaiQuy-Mac.local>
2017-03-27 14:53:29 +0700

    Create list coupon layout

--- app/src/main/java/main/ui/fragment/CouponListFragment.java
+++ app/src/main/java/main/ui/fragment/CouponListFragment.java
@@ -1,15 +1,31 @@
 package main.ui.fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import butterknife.BindArray;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import framework.phvtFragment.BaseFragment;
 import main.R;
 import main.ReloApp;
+import main.ui.adapter.CouponListAdapter;
+import main.ui.model.Coupon;
 import main.util.Constant;
 
 /**
@@ -17,6 +33,19 @@ import main.util.Constant;
  */
 
 public class CouponListFragment extends BaseFragment {
+
+    @BindView(R.id.bt_menu_category)
+    Button btnMenuCategory;
+
+    @BindView(R.id.list_category_listview)
+    ListView lvCategoryMenu;
+
+    @BindArray(R.array.fragment_list_category_coupon)
+    String[] listCategoryCoupon;
+
+    ArrayAdapter<String> itemsAdapter;
+
+    private Unbinder unbinder;
 
     @Override
     public int getRootLayoutId() {
@@ -29,7 +58,49 @@ public class CouponListFragment extends BaseFragment {
         Toolbar toolbar = ButterKnife.findById(root, R.id.toolbar);
         TextView title = ButterKnife.findById(toolbar, R.id.toolbar_title);
         title.setText(R.string.title_coupon_list);
+
+        ArrayList listCoupon = getListData();
+        final ListView lvCategory = ButterKnife.findById(root, R.id.list_category_listview);
+        lvCategory.setAdapter(new CouponListAdapter(getContext(), listCoupon));
+
     }
+
+
+    private ArrayList getListData() {
+        ArrayList<Coupon> results = new ArrayList<Coupon>();
+
+        String categoryName = getResources().getString(R.string.item_coupon_category_name);
+        String companyName = getResources().getString(R.string.item_coupon_company_name);
+        String durationCoupon = getResources().getString(R.string.item_coupon_duration);
+
+        for (int i=0; i<10; i++) {
+            Coupon data = new Coupon();
+            data.setCategoryName(categoryName);
+            data.setCompanyName(companyName);
+            data.setDurationCoupon(durationCoupon);
+            results.add(data);
+        }
+
+        // Add some more dummy data for testing
+        return results;
+    }
+
+    @OnClick(R.id.bt_menu_category)
+    public void clickCategoryMenu(){
+        //Set Style for PopupMenu
+        Context wrapper = new ContextThemeWrapper(getContext(), R.style.popup_menu);
+        PopupMenu popup = new PopupMenu(wrapper, btnMenuCategory);
+        popup.getMenuInflater().inflate(R.menu.popup_categories, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(getActivity(),"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        popup.show();//showing popup menu
+    }
+
 
     @Override
     protected void registerEventHandlers() {
@@ -46,4 +117,10 @@ public class CouponListFragment extends BaseFragment {
             app.trackingAnalytics(Constant.GA_POPULAR_COUPON_SCREEN);
         }
     }
+
+    /*@Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }*/
 }
