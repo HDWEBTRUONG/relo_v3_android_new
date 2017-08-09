fb86a7818ba3747a3c56325ed878e0610b3ae7ae
Ton Van Khanh <khanhton@bravesoft.vn>
2017-08-09 11:44:48 +0700

    Sort Category

--- app/src/main/java/jp/relo/cluboff/util/Constant.java
+++ app/src/main/java/jp/relo/cluboff/util/Constant.java
@@ -108,4 +108,5 @@ public class Constant {
     public static final String TARGET_PUSH_SCREEN_LIST = "list";
 
     public static final String[] listCategory = {"25","27","32","126"};
+    public static final String[] listCategoryName = {"グルメ","カラオケ","レジャー","日帰り湯","その他"};
 }
